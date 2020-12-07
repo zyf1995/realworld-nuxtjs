d095dc112173bf003ac0b527561f574b0e93ed94
zyf <970695936@qq.com>
2020-12-07 09:38:39 +0800

    更新测试

--- api/article.js
+++ api/article.js
@@ -14,11 +14,7 @@ export const getYourFeedArticles = params => {
   return request({
     method: 'GET',
     url: '/api/articles/feed',
-    params,
-    // headers: {
-    //   // 添加用户身份，数据格式：Token空格Token数据
-    //   Authorization: `Token eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NDgxMTYsInVzZXJuYW1lIjoibHB6OTk5IiwiZXhwIjoxNTk3NzQxNTA4fQ.2yO8Fss4hYnvsIN2UYHsutQ1hmYqSSAA-UrIRnP4DOY`
-    // }
+    params
   })
 }
 
